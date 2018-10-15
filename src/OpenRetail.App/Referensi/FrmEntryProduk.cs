5892acc4899187c3dcdfd562bd38613518860fa9
Wira Sandy <wira.sandy@gmail.com>
2018-10-16 06:16:23 +0800

    Disable edit Stok Produk
    Disable input stok produk saat edit produk. Perubahan stok hanya dilakukan melalui menu penyesuaian stok

--- src/OpenRetail.App/Referensi/FrmEntryProduk.cs
+++ src/OpenRetail.App/Referensi/FrmEntryProduk.cs
@@ -96,7 +96,14 @@ namespace OpenRetail.App.Referensi
             txtDiskon.Text = this._produk.diskon.ToString();
             txtStok.Text = this._produk.stok.ToString();
             txtStokGudang.Text = this._produk.stok_gudang.ToString();
-            txtMinStokGudang.Text = this._produk.minimal_stok_gudang.ToString();            
+            txtMinStokGudang.Text = this._produk.minimal_stok_gudang.ToString();
+	    
+	    // Disable input saat edit produk
+	    if (this._isNewData == true)
+	    {
+	        txtStok.Enabled = false;
+		txtStokGudang.Enabled = false;
+	    }
         }
 
         private void LoadInputGrosir()
